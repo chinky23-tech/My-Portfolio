50e379508627fd9ef76e95e1fe2c39f7b979edac
chinky23-tech <chinky0330@gmail.com>
2025-06-01 15:44:09 +0530

    added btn functionality

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoadeed",function(){
+const  button = document.getElementById("viewWork");
+if(button){
+    button.addEventListener("click",function (){
+window.location.href= "portfolio.html";
+    });
+}
+});
